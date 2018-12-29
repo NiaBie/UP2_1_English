87ccd877ecbe329cdaabdfcc725a8226f782532b
171240525-Ling Chenyu <1197225628@qq.com>
2018-12-29 09:26:08 +0800

    all socre

--- word_sord.py
+++ word_sord.py
@@ -1,7 +1,6 @@
-import functools,sys,logging,random,codecs,os
+# -*- coding: utf-8 -*- 
+import functools,sys,logging,random,codecs,time
 from collections import Iterable,Iterator
-from types import MethodType,FunctionType
-from enum import Enum
 
 def array(row,line=0,num=0):
   if line==0:
@@ -29,33 +28,38 @@ def pf_all(words):
 
 words=[]
 problem=[]
-suffix = input('problem suffix:')
-num_start=int(input('start file ord:'))
-num_end=int(input('end file ord:'))
-while num_start <= num_end:
-  num_part = 1
-  name = ("%d_%d" % (num_start, num_part))
-  # print(name)
-  while (os.access(name, os.F_OK) == True):
-    print("add %s" % name)
-    file=open(name,'r')
-    words+=file.readlines()
-    file=codecs.open(name+suffix,'r',encoding='utf-8')
-    problem+=file.readlines()
-    num_part += 1
-    name = ("%d_%d" % (num_start, num_part))
-  num_start += 1
+name=input('输入文件名(s for end):')
+file=open(name,'r')
+while name!='s':
+  file=open(name,'r')
+  words+=file.readlines()
+  file=codecs.open(name+'_p','r',encoding='utf-8')
+  problem+=file.readlines()
+  name=input('输入文件名(s for end):')
 length=len(words)
-# print(len(words))
-# pf_all(words)
+print('总单词数为:',length)
+times=length*4
+print('一共%d次'%(times))
+#times=input('输入次数:')
+#times=int(times)
+#head=input('输入开始(1开始):')
+#tail=input('输入结束:')
+#head=int(head)
+#tail=int(tail)
 exchange(words, problem)
-# pf_all(words)
-print('total words:',length)
+head=1
+tail=length
+print('总单词数为:',length)
+# tm_year, tm_mon, tm_mday, tm_hour, tm_min, tm_sec
+total_score = 0
+start_time = time.localtime()
+start_score = (start_time[3]*60 + start_time[4])*60 + start_time[5]
 for i in range(length):
   k=0
   temp=len(words[i])-1
   str=''
   while not(str==words[i] or str=='s\n'):
+    total_score = int(total_score/2)
     str=input(problem[i])+'\n'
     if k<temp:
       #k+=1
@@ -64,5 +68,8 @@ for i in range(length):
       for l in range(k):
         print(words[i][l],end='')
       print()
+  total_score += 100
   print('next!')
-print('finish!')
+end_time = time.localtime()
+end_score = (end_time[3]*60 + end_time[4])*60 + end_time[5]
+print("score:%d" % (total_score - end_score + start_score))
--- word_sral.py
+++ word_sral.py
@@ -1,6 +1,7 @@
-# -*- coding: utf-8 -*- 
-import functools,sys,logging,random,codecs,time
+import functools,sys,logging,random,codecs,os,time
 from collections import Iterable,Iterator
+from types import MethodType,FunctionType
+from enum import Enum
 
 def array(row,line=0,num=0):
   if line==0:
@@ -28,32 +29,32 @@ def pf_all(words):
 
 words=[]
 problem=[]
-name=input('输入文件名(s for end):')
-file=open(name,'r')
-while name!='s':
-  file=open(name,'r')
-  words+=file.readlines()
-  file=codecs.open(name+'_p','r',encoding='utf-8')
-  problem+=file.readlines()
-  name=input('输入文件名(s for end):')
+suffix = input('problem suffix:')
+num_start=int(input('start file ord:'))
+num_end=int(input('end file ord:'))
+while num_start <= num_end:
+  num_part = 1
+  name = ("%d_%d" % (num_start, num_part))
+  # print(name)
+  while (os.access(name, os.F_OK) == True):
+    print("add %s" % name)
+    file=open(name,'r')
+    words+=file.readlines()
+    file=codecs.open(name+suffix,'r',encoding='utf-8')
+    problem+=file.readlines()
+    num_part += 1
+    name = ("%d_%d" % (num_start, num_part))
+  num_start += 1
 length=len(words)
-print('总单词数为:',length)
-times=length*4
-print('一共%d次'%(times))
-#times=input('输入次数:')
-#times=int(times)
-#head=input('输入开始(1开始):')
-#tail=input('输入结束:')
-#head=int(head)
-#tail=int(tail)
+# print(len(words))
+# pf_all(words)
 exchange(words, problem)
-head=1
-tail=length
-print('总单词数为:',length)
+# pf_all(words)
 # tm_year, tm_mon, tm_mday, tm_hour, tm_min, tm_sec
 total_score = 0
 start_time = time.localtime()
 start_score = (start_time[3]*60 + start_time[4])*60 + start_time[5]
+print('total words:',length)
 for i in range(length):
   k=0
   temp=len(words[i])-1
@@ -68,7 +69,7 @@ for i in range(length):
       for l in range(k):
         print(words[i][l],end='')
       print()
-  total_score += 10
+  total_score += 100
   print('next!')
 end_time = time.localtime()
 end_score = (end_time[3]*60 + end_time[4])*60 + end_time[5]
