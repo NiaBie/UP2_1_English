a360930c88b5b29fd5cafe9c194b5cb30c1c649f
171240525-Ling Chenyu <1197225628@qq.com>
2019-01-01 08:33:42 +0800

    display

--- ran_w.py
+++ ran_w.py
@@ -44,13 +44,16 @@ while head < length:
   temp=len(words[j])-1
   str=input(problem[j])+'\n'
   if not(str==words[j] or str=='S\n'):
-    wrong_time[j] += 1
-    if k<temp:
-      #k+=1
-      k=temp
-    for l in range(k):
-      print(words[j][l],end='')
-    print()
+    while not(str==words[j] or str=='S\n'):
+      wrong_time[j] += 1
+      if k<temp:
+        #k+=1
+        k=temp
+      for l in range(k):
+        print(words[j][l],end='')
+      print()
+      str=input(problem[j])+'\n'
+    print('next-')
   else:
     temp = words[j]
     words[j] = words[head]
@@ -63,6 +66,7 @@ while head < length:
     wrong_time[head] = temp
     head += 1
     print('next!')
+# display
 for i in range(length):
   if wrong_time[i] != 0:
     print("\033[32m%s\033[34;1m%-20s\033[31;1m%10d\033[0m" % (problem[i], words[i][:(len(words[i]) - 1)], wrong_time[i]))
